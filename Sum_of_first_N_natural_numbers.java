d315a05fd809b34503ddf99e5c573cc143a557d9
Saimanikantakoppireddy <147487631+Saimanikantakoppireddy@users.noreply.github.com>
2024-07-31 15:49:19 +0530

    import java.util.Scanner; public class modulus{     public static void main(String[] args){         Scanner read= new Scanner (System.in);         int n;         n=read.nextInt();         System.out.println(n*(n+1)/2);     } }

---
+++ Sum_of_first_N_natural_numbers.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class modulus{
+    public static void main(String[] args){
+        Scanner read= new Scanner (System.in);
+        int n;
+        n=read.nextInt();
+        System.out.println(n*(n+1)/2);
+    }
+}
