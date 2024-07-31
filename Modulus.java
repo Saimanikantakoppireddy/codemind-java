daac12a800acb6a2b33fbcca7af956c1362e8745
Saimanikantakoppireddy <147487631+Saimanikantakoppireddy@users.noreply.github.com>
2024-07-31 15:15:35 +0530

    import java.util.Scanner; public class modulus{     public static void main(String[] args){         Scanner read = new Scanner (System.in);         int a,b;         a=read.nextInt();         b=read.nextInt();         System.out.println(a % b);     } }

---
+++ Modulus.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class modulus{
+    public static void main(String[] args){
+        Scanner read = new Scanner (System.in);
+        int a,b;
+        a=read.nextInt();
+        b=read.nextInt();
+        System.out.println(a % b);
+    }
+}
